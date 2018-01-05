a73eb7725c6422db2c908213c5832b392339db90
Ben French <ben.jminw@gmail.com>
2018-01-04 21:31:25 -0700

    drawEventMaker function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,31 @@
+var userLocation;
+
 var map, infoWindow;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('mapDiv'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 6
+        center: {
+            lat: 39.739,
+            lng: -104.9903
+        },
+        zoom: 8
     });
     infoWindow = new google.maps.InfoWindow;
-    
+
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
-            
+            userLocation = pos;
+
             infoWindow.setPosition(pos);
             infoWindow.setContent('Location found.');
             infoWindow.open(map);
             map.setCenter(pos);
-        }, function() {
+        }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
     } else {
@@ -32,5 +39,30 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
+    infoWindow.open(map);
+}
+
+function drawEventMarker(name, description, lat, lon) {
+    var contentString = `<div id="content">
+    <h1 id="firstHeading" class="firstHeading">${name}</h1>
+    <div id="bodyContent">
+    <p>${description}</p>
+    </div>
+    </div>`;
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: {
+            lat: lat,
+            lng: lon
+        },
+        map: map,
+        title: name
+    });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
 }
